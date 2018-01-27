e21aaf6194063ab9355b9401066cb27f60770da7
budiryan <bryanaa@connect.ust.hk>
2018-01-27 17:17:15 +0800

    update readme and modify some models

--- glucosetracker/glucoses/models.py
+++ glucosetracker/glucoses/models.py
@@ -105,6 +105,75 @@ class Glucose(TimeStampedModel):
         ordering = ['-record_date', '-record_time']
 
 
+class ColorManager(models.Manager):
+
+    def by_user(self, user, **kwargs):
+        """
+        Filter objects by the 'user' field.
+        """
+        return self.select_related().filter(user=user)
+
+    def by_date(self, start_date, end_date, user, **kwargs):
+        """
+        Filter objects by date range.
+        """
+        resultset = self.by_user(user).filter(
+            record_date__gte=start_date,
+            record_date__lte=end_date,
+        )
+
+        return resultset.order_by('-record_date', '-record_time')
+
+    def by_category(self, start_date, end_date, user, **kwargs):
+        """
+        Group objects by category and take the count.
+        """
+        data = self.by_date(start_date, end_date, user)
+
+        return data.values('category__name')\
+            .annotate(count=models.Count('value'))\
+            .order_by('category')
+
+    def avg_by_category(self, start_date, end_date, user):
+        """
+        Group objects by category and take the average of the values.
+        """
+        data = self.by_date(start_date, end_date, user)
+
+        return data.values('category__name')\
+            .annotate(avg_value=models.Avg('value'))\
+            .order_by('category')
+
+    def avg_by_day(self, start_date, end_date, user):
+        """
+        Group objects by record date and take the average of the values.
+        """
+        data = self.by_date(start_date, end_date, user)
+
+        return data.values('record_date')\
+            .annotate(avg_value=models.Avg('value'))\
+            .order_by('record_date')
+
+
+
+class Color(TimeStampedModel):
+    objects = ColorManager()
+
+    user = models.ForeignKey(User)
+    value = models.PositiveIntegerField(validators=[MaxValueValidator(54054),
+                                                    MinValueValidator(0)])
+    record_date = models.DateField('Date')
+    record_time = models.TimeField('Time')
+    notes = models.TextField('Notes', null=False, blank=True, default='')
+    tags = TaggableManager(blank=True, help_text=None)
+
+    def __unicode__(self):
+        return str(self.value)
+
+    class Meta:
+        ordering = ['-record_date', '-record_time']
+
+
 class Category(models.Model):
     name = models.CharField(unique=True, max_length=255)
 
